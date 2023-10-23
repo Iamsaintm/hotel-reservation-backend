44a56104996cb7b85d524e9515f926e929f4fe2a
Iamsaintm <p.leelayuthto@hotmail.com>
2023-10-23 20:17:35 +0700

    can search room vacant

--- src/controllers/user/room-controller.js
+++ src/controllers/user/room-controller.js
@@ -1,21 +1,23 @@
-const prisma = require("../../models/prisma");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 const createError = require("../../utils/create-error");
 
 exports.getRoom = async (req, res, next) => {
   try {
     const data = req.query;
+
     if (!data.startDate || !data.endDate || !data.guestLimit) {
       return next(createError("All search parameters are required", 400));
     }
 
     const startDate = new Date(data.startDate);
     const endDate = new Date(data.endDate);
+
     if (startDate >= endDate) {
       return next(
         createError("Start date cannot be greater than end date", 400)
       );
     }
-
     const rooms = await prisma.room.findMany({
       where: {
         roomType: {
@@ -24,43 +26,76 @@ exports.getRoom = async (req, res, next) => {
         isMaintenance: false,
       },
       include: {
-        bookings: {
-          where: {
-            AND: [
-              {
-                startDate: { lte: endDate },
-                endDate: { gte: startDate },
-              },
-              {
-                isPayment: { in: ["ACCEPT", "REJECT"] },
-              },
-            ],
-          },
-        },
+        bookings: true,
         roomType: true,
       },
     });
 
-    const availableRooms = rooms.filter((room) => {
-      const booking = room.bookings.some((booking) => {
-        const bookingStartDate = new Date(booking.startDate);
-        const bookingEndDate = new Date(booking.endDate);
-
-        return (
-          bookingStartDate <= endDate &&
-          bookingEndDate >= startDate &&
-          booking.isPayment === "REJECT"
-        );
-      });
-
-      return !booking;
+    const roomNotVacant = await prisma.room.findMany({
+      where: {
+        bookings: {
+          some: {
+            AND: [
+              {
+                OR: [
+                  {
+                    startDate: {
+                      lte: startDate,
+                      gte: startDate,
+                    },
+                  },
+                  {
+                    endDate: {
+                      lte: startDate,
+                      gte: startDate,
+                    },
+                  },
+                  {
+                    startDate: {
+                      lte: startDate,
+                      gte: startDate,
+                    },
+                    endDate: {
+                      lte: startDate,
+                      gte: startDate,
+                    },
+                  },
+                  {
+                    startDate: {
+                      gte: startDate,
+                      lte: startDate,
+                    },
+                    endDate: {
+                      gte: startDate,
+                      lte: startDate,
+                    },
+                  },
+                ],
+              },
+              {
+                isPayment: {
+                  in: ["PENDING", "ACCEPT"],
+                },
+              },
+            ],
+          },
+        },
+      },
+      include: {
+        bookings: true,
+        roomType: true,
+      },
     });
 
-    if (availableRooms.length === 0) {
-      return next(createError("No room available", 400));
-    }
+    const roomNotVacantIds = roomNotVacant.map((room) => room.id);
 
-    res.status(200).json((room = availableRooms));
+    const filteredRooms = rooms.filter(
+      (room) => !roomNotVacantIds.includes(room.id)
+    );
+
+    console.log(filteredRooms);
+
+    res.status(200).json(filteredRooms);
   } catch (err) {
     next(err);
   }
